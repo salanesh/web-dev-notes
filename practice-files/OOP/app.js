bbd58c0e196218f7d202b0fbe9fe809c3727c4b9
Joseph Sala <joseph.sala2001@gmail.com>
2023-01-29 21:14:35 +0800

    finished with OOP

--- practice-files/OOP/app.js
+++ practice-files/OOP/app.js
@@ -59,7 +59,7 @@ constructColor.prototype.hex = function () {
 };
 // MAKING A METHOD THAT REQUIRES A PARAMETER NOT INCLUDED WITH THE CONSTRUCTION FUNCTION
 // IT ALSO HAS A DEFAULT VALUE OF 1
-constructColor.prototype.rgba = function (a = 1) {
+constructColor.prototype.rgba = function (a = 1.0) {
   const { r, g, b } = this;
   return `rgba(${r},${g},${b},${a})`;
 };
@@ -71,3 +71,68 @@ constructColor.prototype.printIt = function () {
 // WHEN MAKING AN OBJECT WITH A CONSTRUCTION FUNCTION USE THE "new" KEYWORD
 const secondColor = new constructColor(123, 45, 67);
 const thirdColor = new constructColor(255, 120, 93);
+
+// CLASSES
+// CLASSES DO THE SAME THING AS CONSTRUCTOR FUNCTIONS BUT ARE MORE CONVENIENT SYNTACTICALLY
+// THE METHODS INSIDE THE PROTOTYPE DOES NOT HAVE TO BE CREATED OUTSIDE THE CLASS, SO IT IS CLEANER
+// THE "class" keyword needs to be used to declare a class
+// THE "constructor" keyword needs to be used inside of the class to create and run the constructor function
+
+class truColor {
+  constructor(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+  rgb() {
+    const { r, g, b } = this;
+    return `rgb(${r},${g},${b})`;
+  }
+  rgba(a = 1.0) {
+    const { r, g, b } = this;
+    return `rgba(${r},${g},${b},${a})`;
+  }
+  hex() {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
+  }
+  printIt(x) {
+    document.body.style.backgroundColor = this.rgba(x);
+  }
+}
+
+const fourthColor = new truColor(55, 200, 123);
+const fifthColor = new truColor(255, 10, 10);
+
+// EXTENDING A CLASS AND THE SUPER KEYWORD
+class Pet {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  eat() {
+    const { name } = this;
+    console.log(`${name} is eating. om nom nom :)`);
+  }
+}
+
+class Dog extends Pet {
+  bark() {
+    const { name } = this;
+    console.log(`${name} says WOOF! WOOF! WOOF!`);
+  }
+}
+
+class Cat extends Pet {
+  constructor(name, age, lives = 9) {
+    super(name, age);
+    this.lives = lives;
+  }
+  meow() {
+    const { name } = this;
+    console.log(`${name} is going meow :X`);
+  }
+}
+
+const scooby = new Dog("Scooby", 8);
+const dani = new Cat("Yriwpoqjwwbj", 15);
